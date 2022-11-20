b8da2bc05d16776fbf69b5ea1993f8c0d0a8f52e
emmecche <patrickfisler10@gmail.com>
2022-11-20 03:02:59 -0600

    script ok, may revisit error handling, need style

--- Lab4/script.js
+++ Lab4/script.js
@@ -6,41 +6,74 @@ window.addEventListener('DOMContentLoaded', function() {
     input_line = this.document.getElementById("location-entry");
     enter_button = this.document.getElementById("enter");
     location_button = this.document.getElementById("location-button");
+
+    current_forecast = this.document.getElementById("current-day");
+    next_forecast    = this.document.getElementById("next-7days");
     
+    forecast_region   = this.document.getElementById("forecast-region");
     forecast_icon     = this.document.getElementById("forecast-icon");
+    forecast_temp     = this.document.getElementById("forecast-temp");
     forecast_dayhour  = this.document.getElementById("forecast-dayhour");
     forecast_comment  = this.document.getElementById("forecast-comment");
     forecast_precip   = this.document.getElementById("forecast-precip");
     forecast_humidity = this.document.getElementById("forecast-humidity");
     forecast_wind     = this.document.getElementById("forecast-wind");
 
-    function pruneLocation(x) {
-        x = x.replace(/ /g, '');
-        return x.toLowerCase();
+    next_7days = this.document.getElementsByClassName("mini-forecast");
+
+    function pruneLocation(inp) {
+        inp = inp.replace(/ /g, '');
+        return inp.toLowerCase();
     }
 
     function connectWeatherInput() {
         loc = pruneLocation(input_line.value);
         fetch(`https://weatherdbi.herokuapp.com/data/weather/${loc}`)
             .then(res=>res.json())
-            .then(function(jsonData){
-                console.log(jsonData.currentConditions.dayhour)
+            .then(function(jsonData) {
+                displayWeather(jsonData)
             })
-            .catch();
+            .catch(function() {
+                current_forecast.innerHTML = "Invalid Location";
+            });
     }
 
     function connectWeatherButton(lat, long) {
         fetch(`https://weatherdbi.herokuapp.com/data/weather/${lat},${long}`)
             .then(res=>res.json())
-            .then(function(jsonData){
-                forecast_icon.src = jsonData.currentConditions.iconURL; 
-                forecast_dayhour.value  = jsonData.currentConditions.dayhour;
-                forecast_comment.value  = jsonData.currentConditions.comment;
-                forecast_precip.value   = jsonData.currentConditions.precip;
-                forecast_humidity.value = jsonData.currentConditions.humidity;
-                forecast_wind.value     = jsonData.currentConditions.wind.mile + " mph";
+            .then(function(jsonData) {
+                displayWeather(jsonData)
             })
             .catch();
+    }
+
+    function displayWeather(jsonData) {
+        showHidden();
+
+        forecast_region.innerHTML = jsonData.region;
+
+        forecast_icon.src = jsonData.currentConditions.iconURL;
+        forecast_temp.innerHTML     = jsonData.currentConditions.temp.f + "° Fahrenheit";
+        forecast_dayhour.innerHTML  = jsonData.currentConditions.dayhour;
+        forecast_comment.innerHTML  = jsonData.currentConditions.comment;
+        forecast_precip.innerHTML   = jsonData.currentConditions.precip;
+        forecast_humidity.innerHTML = jsonData.currentConditions.humidity;
+        forecast_wind.innerHTML     = jsonData.currentConditions.wind.mile + " mph";
+
+        next_days_arr = jsonData.next_days;
+        for (var i=0; i<next_7days.length; i++) {
+            mini_day = next_7days[i].getElementsByClassName("mini-day")[0];
+            console.log(mini_day, next_days_arr[i]);
+            mini_day.innerHTML = next_days_arr[i+1].day;
+            mini_icon = next_7days[i].getElementsByClassName("mini-icon")[0];
+            mini_icon.src = next_days_arr[i+1].iconURL;
+            mini_max = next_7days[i].getElementsByClassName("max&min")[0].getElementsByClassName("mini-max")[0];
+            mini_max.innerHTML = next_days_arr[i+1].max_temp.f + "° Fahrenheit";
+            mini_min = next_7days[i].getElementsByClassName("max&min")[0].getElementsByClassName("mini-min")[0];
+            mini_min.innerHTML = next_days_arr[i+1].min_temp.f + "° Fahrenheit";
+            mini_comment = next_7days[i].getElementsByClassName("mini-comment")[0];
+            mini_comment.innerHTML = next_days_arr[i+1].comment;
+        }
     }
 
     function getLocation() {
@@ -56,6 +89,11 @@ window.addEventListener('DOMContentLoaded', function() {
         "\nLongitute: " + position.coords.longitude);
 
         connectWeatherButton(position.coords.latitude, position.coords.longitude);
+    }
+
+    function showHidden() {
+        current_forecast.style.display = "block";
+        next_forecast.style.display = "block";
     }
 
     location_button.addEventListener("click", getLocation, false);
